df9c8f1349cce10ab3927c510228a8224eda675b
a-jinkya18 <ajinkyajadhav0179@gmail.com>
2025-06-13 09:00:51 +0530

    added

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
 // src/store/store.js
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/authSlice';
-import bridgeReducer from '../features/bridgeSlice';
+import bridgeReducer from '../features/BridgeSlice';
 
 export const store = configureStore({
   reducer: {
